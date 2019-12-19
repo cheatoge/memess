2492813c4d14b8e3b8daa7d8f1879dc45c2c7a57
Łukasz Szpil <cheatoge@gmail.com>
2019-12-19 20:46:04 +0100

    Display newlines in text memes

--- src/components/Meme/TextMeme.js
+++ src/components/Meme/TextMeme.js
@@ -3,9 +3,19 @@ import './TextMeme.css'
 
 class TextMeme extends React.Component {
   render() {
+    let fragments = this.props.text.split('\n')
+    fragments = fragments.map(fragment => {
+      return (
+        <>
+          {fragment}
+          <br />
+        </>
+      )
+    })
+    
     return (
       <div className='textMeme'>
-        {this.props.text}
+        {fragments}
       </div>
     )
   }
